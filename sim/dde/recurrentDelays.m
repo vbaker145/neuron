eda58a78b6242dc53bc4d7916dccfcd4282264a8
VB <vincent.j.baker@lmco.com>
2017-07-25 16:41:16 -0400

    More updates

--- sim/dde/recurrentDelays.m
+++ sim/dde/recurrentDelays.m
@@ -1,6 +1,6 @@
 %Simulate recurrent network with delays
 clear all; close all;
-N = 50;
+N = 30;
 maxDelay = 33;
 
 delays = randi(maxDelay, N); 
@@ -15,7 +15,7 @@ x = zeros(maxDelay, N); %Node activations, first column is input history
 win = 0.1.*(rand(1,N)-0.5);
 w = rand(N,N)-0.5;
 %u = sin(1.1.*(1:(initLen+trainLen+testLen)));
-u = mso(1,initLen+trainLen+testLen,3);
+u = mso(1,initLen+trainLen+testLen,8);
 
 for kk=1:N
    colInd = 1:N;
@@ -41,9 +41,13 @@ Yt = u(initLen+1:trainLen+initLen); %Training signal
 reg = 1e-8;  % regularization coefficient
 X = out;
 X_T = out';
-Wout = Yt*X_T * inv(X*X_T + reg*eye(2+N));
+%Wout = Yt*X_T * inv(X*X_T + reg*eye(2+N));
+%Wout = rand(1,N+2)-0.5;
+Wout = Yt*pinv(X);
+
+inData =u(initLen+trainLen+1); 
 for jj=1:testLen
-    inData =u(initLen+trainLen+jj); 
+   inData =u(initLen+trainLen+jj); %Predictive mode
    %Copy down activation array
    x(2:end, :) = x(1:end-1,:);
    for kk=1:N
@@ -52,13 +56,17 @@ for jj=1:testLen
       x(1,kk) = tanh(w(kk,:)*(x(inds(:,kk)))+win(kk)*inData);
    end
    Xout(jj) = Wout*[1; inData; x(1,:)'];
-   
+   %inData = Xout(jj); %Generative mode
+   %inData = rand();
 end
-figure; imagesc(out); colorbar
+figure(10); imagesc(out); colorbar
 
-figure; subplot(2,1,1);
+figure(20); subplot(2,1,1);
 gt = u(trainLen+initLen+1:end);
 plot(Xout); hold on; plot(gt,'r');
 subplot(2,1,2);
 err = Xout-gt;
 plot(err);
+
+figure(30); plot(20*log10(abs(fftshift(fft(gt)))));
+hold on; plot(20*log10(abs(fftshift(fft(Xout)))),'g');
