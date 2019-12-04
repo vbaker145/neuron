bedc65ca055fda94a64d439fc4414ec0a2a09ef5
Vince Baker <vjb42@drexel.edu>
2019-12-03 22:22:02 -0500

    Added wave density/initiation correlation back in, unified final single LTS experiment for both initiation and density

--- sim/lsm/NoConnectionWaveSim.m
+++ sim/lsm/NoConnectionWaveSim.m
@@ -1,3 +1,4 @@
+%% Set up base column
 clear; close all;
 width = 2;
 height = 2;
@@ -38,11 +39,9 @@ st = zeros(N, size(t,2));
 st(ecn,1:1/dt:end) = stimStrength*rand(sum(ecn),tmax+1);
 st(~ecn,1:1/dt:end) = stimStrength*(2/5)*rand(sum(~ecn),tmax+1);
 sti = (interp1(0:tmax, st(:,1:1/dt:end)', 0:dt:tmax))';
-blankInterval = floor( size(sti,2)/8 );
-blanking = ones(size(sti));
-blanking(:, 2*blankInterval:3*blankInterval) = 0;
-%sti = sti.*blanking;
-        
+
+%% Stimulate base column, plot firing activity
+
 [v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, sti);
 size(firings)
 
@@ -62,7 +61,7 @@ for jj=1:N
    end
 end
 
-%Simulate same column with no connections
+%% Simulate same column with no connections
 S = zeros(size(S)); 
 
 st = zeros(N, size(t,2));
@@ -85,3 +84,40 @@ fscale = firings(:,2)/(width*height);
 barh(edges(1:end-1)+edges(2)/2, bins./sum(bins), 'k');
 xlabel('Fraction of total firing events'); ylabel('Z position');
 set(gca, 'FontSize', 12);
+
+
+%% Single strong inhibitory neuron test
+connectivity.percentExc = 40*structure.width*structure.height; %Single LTS inhibitory neuron
+connectivity.connStrength = 12;
+
+[a,b,c,d, S, delays, ecn] = makeColumnParameters(structure, connectivity, delay);
+
+vinit=-65*ones(N,1)+5*rand(N,1);    % Initial values of v
+uinit=b.*vinit;                 % Initial values of u
+
+[v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, sti);
+
+figure(30); subplot(1,3,1); 
+plot(firings(:,1)./1000, firings(:,2)/(width*height),'k.');
+xlabel('Time (seconds)'); ylabel('Z position');
+set(gca, 'FontSize', 12);
+
+stImpulse = zeros(N, size(t,2))*rand();
+sidx = 1;
+stimDuration = floor(20/dt);
+stimDepth = 5;
+stImpulse(sidx:stimDepth*(sidx+width*height),20:(20+stimDuration))= 10;
+stImpulse = (interp1(0:tmax, stImpulse(:,1:1/dt:end)', 0:dt:tmax))';
+
+[v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, stImpulse);
+figure(30); subplot(1,3,2); 
+plot(firings(:,1)./1000, firings(:,2)/(width*height),'k.');
+xlabel('Time (seconds)'); 
+set(gca, 'FontSize', 12);
+
+nbins = 50;
+fscale = firings(:,2)/(width*height);
+[bins, edges] = histcounts(fscale, 0:layers/nbins:layers );
+subplot(1,3,3); barh(edges(1:end-1)+edges(2)/2, bins./sum(bins),'k')
+xlabel('Fraction of total firing events', 'FontSize', 12)
+
