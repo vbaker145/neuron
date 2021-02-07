f732991269d9808c9db8cd97f42240def1171c75
Vince Baker <vjb42@drexel.edu>
2021-02-07 17:07:19 -0500

    Results on firing rate encoding into traveling wave rate

--- sim/audio/FiringRateEncoding.m
+++ sim/audio/FiringRateEncoding.m
@@ -46,38 +46,58 @@ for cidx = 1:length(colSep)
     cs.structure.columnSpacing = colSep(cidx);
     colStructs = [colStructs cs];
 end
-firingRates = [1 2 10 20];
+firingRates = 1:2:21;
 nFiringRates = length(firingRates);
+
+nTrials = 10;
 
 %connErr = zeros(length(colStructs), nTrials, 6);
 %nFirings = zeros(length(colStructs), nTrials);
 
 for fr = 1:nFiringRates
     fr
-    
-    %Random stimulus and background
-    firingRate = firingRates(fr)*ones(1,length(t));
-    [st, stSpikes] = firingRateEnsembleStimulus( colStructBase.structure, ...
-                                        colStructBase.csec, colStructBase.ecn, dt, ...
-                                        t, nInputPool, firingRate, 6 );
 
     %% Simulate column ensembles
     for connIdx =1:length(colStructs)
-        colStruct = colStructs(connIdx);
+        for trial=1:nTrials
+            colStruct = colStructs(connIdx);
+            
+            %Random stimulus and background
+            firingRate = firingRates(fr)*ones(1,length(t));
+            [st, stSpikes] = firingRateEnsembleStimulus( colStructBase.structure, ...
+                                                colStructBase.csec, colStructBase.ecn, dt, ...
+                                                t, nInputPool, firingRate, 6 );
 
-        vinit=-65*ones(colStruct.N,1)+0*rand(colStruct.N,1);    % Initial values of v
-        uinit=(colStruct.b).*vinit;                 % Initial values of u
+            vinit=-65*ones(colStruct.N,1)+0*rand(colStruct.N,1);    % Initial values of v
+            uinit=(colStruct.b).*vinit;                 % Initial values of u
 
-        %Connected minicolumn ensemble
-        [v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), ...
-            colStruct.a, colStruct.b, colStruct.c, colStruct.d, colStruct.S, ...
-            colStruct.delays, st);  
-        size(firings)
+            %Simulate column ensemble
+            [v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), ...
+                colStruct.a, colStruct.b, colStruct.c, colStruct.d, colStruct.S, ...
+                colStruct.delays, st);  
+            size(firings)
 
-        figure(10); subplot(length(colStructs), nFiringRates,(connIdx-1)*nFiringRates+fr);
-        plot(firings(:,1) ,firings(:,2)./colStruct.Nlayer, 'k.')
-        title(num2str(firingRates(fr)));
-        set(gca,'FontSize', 12);
+            figure(10); subplot(length(colStructs), nFiringRates,(connIdx-1)*nFiringRates+fr);
+            plot(firings(:,1)  ,firings(:,2)./colStruct.Nlayer, 'k.')
+            if connIdx == 1
+                title(num2str(firingRates(fr)));
+                set(gca, 'XTick', []);
+            end
+            set(gca,'FontSize', 12);
+
+            %Record # of peaks
+            inputMP = mean(vall(1:colStruct.Nlayer,:));
+            outputMP = mean(vall(end-colStruct.Nlayer:end,:));
+
+            %Find peaks in input/output membrane potential
+            [ip iw op ow] = findPeaks(inputMP, outputMP, dt, 0.25);
+            npks(fr, connIdx, trial) = length(op);
+        end %End trial loop    
     end %End loop columns
-end %End loop over trials
+end %End loop over trial
+
+m = mean(npks,3);
+v = std(npks,0,3);
+figure(20); errorbar(firingRates,m(:,1), v(:,1));
+hold on; errorbar(firingRates,m(:,2), v(:,2));
 
