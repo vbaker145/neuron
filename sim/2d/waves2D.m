f21f821e9d614f09a9f3def935124c1bf994682c
Vince Baker <vjb42@drexel.edu>
2021-07-07 15:43:59 -0400

    Moved 1-D specific Methods into 1-D section, added spiking rate graphic'

--- sim/2d/waves2D.m
+++ sim/2d/waves2D.m
@@ -22,7 +22,7 @@ structure.displacement = 0;
 connectivity.percentExc = 0.8;
 connectivity.connType = 1;
 connectivity.lambda = 2.5;
-connectivity.C = 0.5;
+connectivity.C = 0.3; %Scaling connectivity to 1-D minicolumn
 connectivity.connStrength = 10;
 connectivity.maxLength = 100; 
 
@@ -43,9 +43,9 @@ stimStrength = 10;
 st = zeros(N, size(t,2));
 st(ecn,1:1/dt:end) = stimStrength*rand(sum(ecn),tmax+1);
 st(~ecn,1:1/dt:end) = stimStrength*(2/5)*rand(sum(~ecn),tmax+1);
-sti = (interp1(0:tmax, st(:,1:1/dt:end)', 0:dt:tmax))';
+st = (interp1(0:tmax, st(:,1:1/dt:end)', 0:dt:tmax))';
 
-[v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, sti);
+[v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, st);
 %plot(firings(:,1)./1000, firings(:,2)/(width*height),'k.');
 
 %plotWaves2D( firings, pos, vall, dt, ecn, '2DWaves_1layer.avi' );
