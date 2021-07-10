4209b823a1734793329700c8a1bf636addf39458
Vince Baker <vjb42@drexel.edu>
2021-07-10 09:09:47 -0400

    Spiral waves observed after lowering K from 10 to 6. Kappa also lowered to 0.4

--- sim/2d/waves2D.m
+++ sim/2d/waves2D.m
@@ -1,11 +1,11 @@
 clear; close all;
 
-rng(35); %Seed random for consistent results
+rng(42); %Seed random for consistent results
 
 addpath('../sce/');
 
-width = 100;
-height = 100;
+width = 200;
+height = 200;
 layers = 2;
 N = width*height*layers;
 
@@ -23,11 +23,11 @@ connectivity.percentExc = 0.8;
 connectivity.connType = 1;
 connectivity.lambda = 2.5;
 connectivity.C = 0.5; 
-connectivity.connStrength = 10;
+connectivity.connStrength = 6;
 connectivity.maxLength = 100; 
 
 delay.delayType = 1;
-delay.delayMult = 0.1;
+delay.delayMult = 0.4;
 delay.delayFrac = 1.0;
 delay.dt = dt;
 
@@ -39,7 +39,7 @@ vinit=-65*ones(N,1)+0*rand(N,1);    % Initial values of v
 uinit=b.*vinit;                 % Initial values of u
 
 %Background, corrected for dt
-stimStrength = 10;
+stimStrength = 4;
 st = zeros(N, size(t,2));
 st(ecn,1:1/dt:end) = stimStrength*rand(sum(ecn),tmax+1);
 st(~ecn,1:1/dt:end) = stimStrength*(2/5)*rand(sum(~ecn),tmax+1);
@@ -49,4 +49,4 @@ st = (interp1(0:tmax, st(:,1:1/dt:end)', 0:dt:tmax))';
 %plot(firings(:,1)./1000, firings(:,2)/(width*height),'k.');
 
 %plotWaves2D( firings, pos, vall, dt, ecn, '2DWaves_1layer.avi' );
-plotWaves2D_Frames( firings, pos, vall, dt, 2000:20:2220 );
+plotWaves2D_Frames( firings, pos, vall, dt, 1000:20:1220 );
