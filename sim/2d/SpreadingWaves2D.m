94aa483f7f80c9ae94d6c6d5e6e8e9629c360a6b
Vincent J. Baker <vjb42@drexel.edu>
2021-10-24 20:06:20 -0400

    Better plots for spreading waves, 2D single layer. ISI plot looks similar even though we don't see large scale waves

--- sim/2d/SpreadingWaves2D.m
+++ sim/2d/SpreadingWaves2D.m
@@ -1,6 +1,6 @@
 clear; close all;
 
-rng(35); %Seed random for consistent results
+rng(42); %Seed random for consistent results
 
 addpath('../sce/');
 
@@ -45,7 +45,7 @@ st(ecn,1:1/dt:end) = stimStrength*rand(sum(ecn),tmax+1);
 st(~ecn,1:1/dt:end) = stimStrength*(2/5)*rand(sum(~ecn),tmax+1);
 st = (interp1(0:tmax, st(:,1:1/dt:end)', 0:dt:tmax))';
 
-[v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, st);
+[v, vall, u, uall, firings] =  izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, st);
 %plot(firings(:,1)./1000, firings(:,2)/(width*height),'k.');
 
 %plotWaves2D( firings, pos, vall, dt, ecn, '2DWaves_1layer.avi' );
