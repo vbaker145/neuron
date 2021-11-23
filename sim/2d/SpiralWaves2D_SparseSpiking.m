6b65a91fb6d8c92442c70a119dc5e8d8adbc35e1
Vincent J. Baker <vjb42@drexel.edu>
2021-11-23 17:51:18 -0500

    Sparse spiking experiemnts

--- sim/2d/SpiralWaves2D_SparseSpiking.m
+++ sim/2d/SpiralWaves2D_SparseSpiking.m
@@ -20,7 +20,7 @@ connectivity.percentExc = 0.8;
 connectivity.connType = 1;
 connectivity.lambda = 7.5;
 connectivity.C = 0.1; 
-connectivity.connStrength = 6;
+connectivity.connStrength = 5.5;
 connectivity.maxLength = 100; 
 
 delay.delayType = 1;
@@ -42,9 +42,9 @@ st(ecn,1:1/dt:end) = stimStrength*rand(sum(ecn),tmax+1);
 st(~ecn,1:1/dt:end) = stimStrength*(2/5)*rand(sum(~ecn),tmax+1);
 st = (interp1(0:tmax, st(:,1:1/dt:end)', 0:dt:tmax))';
 
-[v, vall1, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, st);
+[v, vall, u, uall, firings] = izzy_net(vinit,uinit,dt, length(t), a, b, c, d, S, delays, st);
 %plot(firings(:,1)./1000, firings(:,2)/(width*height),'k.');
 
-plotWaves2D_Frames( firings, pos, vall1, dt, 1200:15:1200+11*15 );
+plotWaves2D_Frames( firings, pos, vall, dt, 1200:15:1200+11*15 );
 
 
