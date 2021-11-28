af0b073246af9c68b0331559e75b4eb72d8764c8
Vincent J. Baker <vjb42@drexel.edu>
2021-11-28 09:11:12 -0500

    Spike rate calculations for attractor measurement and wave direction

---
+++ sim/2d/calcSpikeRate.m
@@ -0,0 +1,35 @@
+function [spikeCounts] = calcSpikeRate(f, pos, gridSz, tWin)
+
+xsz = max(pos.x(:));
+ysz = max(pos.y(:));
+
+nXbins = ceil(xsz/gridSz);
+nYbins = ceil(ysz/gridSz);
+
+tMax = max(f(:,1));
+nTbins = ceil(tMax/tWin);
+
+spikeCounts = zeros(nXbins, nYbins, nTbins);
+
+figure(20); 
+for tidx = 1:nTbins
+   tnow = (tidx-1)*tWin;
+   fidx = f(:,1)>=tnow & f(:,1)<(tnow+tWin);
+   posIdx = f(find(fidx),2);
+   xv = floor( pos.x(posIdx)/gridSz )+1;
+   yv = floor( pos.y(posIdx)/gridSz )+1;
+   for kk=1:length(xv)
+       spikeCounts(xv(kk),yv(kk),tidx) = spikeCounts(xv(kk),yv(kk),tidx)+1;
+   end
+   
+   if ~isempty(posIdx)
+        plot(pos.x(posIdx), pos.y(posIdx), 'k.');
+        xlim([0 xsz]); ylim([0 ysz]);
+        title(num2str(tidx));
+   end
+end
+
+figure(50); imagesc(spikeCounts(:,:,end)'); colormap('gray'); colorbar; set(gca, 'YDir', 'Normal');
+ 
+end
+
