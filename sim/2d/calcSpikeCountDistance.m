98e7d209636531f8f2e6f870e2ca2bd2529fc1a8
Vincent J. Baker <vjb42@drexel.edu>
2021-12-27 13:49:48 -0500

    Coorelation with normalization

--- sim/2d/calcSpikeCountDistance.m
+++ sim/2d/calcSpikeCountDistance.m
@@ -8,13 +8,16 @@ Npx = size(spikeCounts,1)*size(spikeCounts,2);
 nSCD = length(scdRange);
 scd = zeros(nSCD, 1);
 scRef = squeeze( spikeCounts(:,:,refIdx) );
-scRef = scRef./sum(scRef(:));
+scRef = scRef - mean(scRef(:));
+scRef = scRef./sqrt(sum(abs(scRef(:))));
+
 
 for ii = 1:nSCD
     scComp = squeeze( spikeCounts(:,:,scdRange(ii)) );
-    scComp = scComp./sum(scComp(:));
+    scComp = scComp-mean(scComp(:));
+    scComp = scComp./sqrt(sum(abs(scComp(:))));
     %scd(ii) = sum( (scRef(:)-scComp(:)).^2 ) / Npx;
-    scd(ii) = sum(abs(scRef(:).*scComp(:)))./Npx;
+    scd(ii) = sum((scRef(:).*scComp(:)))/2;
 end
 
 end
