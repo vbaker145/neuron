b1e4c4baa717735a3876631eaa255528fa7c6fa1
Vincent J. Baker <vjb42@drexel.edu>
2022-01-01 11:26:39 -0500

    Added wave angle/speed calculation from spike timing

---
+++ sim/2d/calcWaveProperties.m
@@ -0,0 +1,69 @@
+function [wt, theta, c] = calcWaveProperties(f, pos, cut, r, tut, waveSpeed)
+
+%Calculate wave angles and speeds 
+% f - matrix of firing indices and times
+% pos - position of neurons
+% cut - cell under test (index of neuron)
+% r - radius within cut to consider
+% tut - time under test, min/max times to consider
+
+%Find distance from cell under test
+x = pos.x(:); y = pos.y(:);
+xut = x(cut); yut = y(cut);
+dis = sqrt((y-yut).^2 + (x-xut).^2);
+
+%Find spikes that fall within distance and time limits
+rwin = find(dis<=r);
+ridx =  ismember(f(:,2), rwin);
+tidx = (f(:,1)>tut & f(:,1)<tut+100);
+cidx = ridx & tidx;
+
+fa = f(cidx, :);
+
+cutSpikes = fa( fa(:,2)==cut,: );
+
+if isempty( cutSpikes )
+    disp('No spikes from CUT');
+    wt = []; theta = []; c = [];
+end
+
+wt = cutSpikes(1,1);
+nidx = unique(fa(:,2));
+
+for jj=1:length(nidx)
+   spikes = fa( fa(:,2)==nidx(jj), :); 
+   dt(jj) = spikes(1,1) - cutSpikes(1,1);
+   spikeVec(jj,:) = [x(spikes(1,2))-xut, y(spikes(1,2))-yut];
+end
+
+%Plot spike timing delta
+figure(10); 
+scatter(spikeVec(:,1), spikeVec(:,2), 30, dt, 'filled');
+colorbar;
+axis equal; 
+
+%Find best fit angle 
+mAngle = angle(spikeVec(:,1) + 1i*spikeVec(:,2));
+mr = sqrt(spikeVec(:,1).^2 + spikeVec(:,2).^2 );
+
+testAngle = (0:364).*pi/180;
+for jj=1:length(testAngle)
+    errAngle(jj) = sum( abs(dt' - (1/waveSpeed)*(mr.*cos(mAngle-testAngle(jj)))) )/length(nidx);
+end
+
+[mv midx] = min(errAngle);
+theta = testAngle(midx);
+
+figure(20);
+plot(errAngle, 'k');
+title(num2str(mv));
+
+%Now find best speed fit at that angle
+testSpeed = 0.1:0.1:2;
+for jj=1:length(testSpeed)
+    errSpeed(jj) = sum( abs(dt' - (1/testSpeed(jj))*(mr.*cos(mAngle-theta))) )/length(nidx);
+end
+[mv midx] = min(errSpeed);
+c = testSpeed(midx);
+end
+
